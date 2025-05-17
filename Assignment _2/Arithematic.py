7939a24d0985a6c41305955ad241b30078785591
Karan Jadhav <142383819+KaranJadhav0027@users.noreply.github.com>
2025-05-17 08:05:32 +0530

    Add files via upload

---
+++ Assignment _2/Arithematic.py
@@ -0,0 +1,13 @@
+def Add(Value1,Value2):
+    return Value1+Value2
+
+def Sub(Value1,Value2):
+    return Value1-Value2
+
+def Mult(Value1,Value2):
+    return Value1*Value2
+
+def Div(Value1,Value2):
+    if Value2==0:
+        print("Enter Greater than Zero Value")
+    return Value1/Value2
