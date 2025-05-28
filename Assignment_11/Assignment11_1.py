ec54808c185959e6a981b38ddee34a24b15ddebd
Karan Jadhav <142383819+KaranJadhav0027@users.noreply.github.com>
2025-05-28 23:58:20 +0530

    Add files via upload

---
+++ Assignment_11/Assignment11_1.py
@@ -0,0 +1,31 @@
+"""############################################################
+   Program  Recursion Function to print 1 to N
+
+Input
+Enter a Number :5
+
+Output
+1
+2
+3
+4
+5
+###############################################################"""
+
+Rec=1
+def Display(no):
+    global Rec
+    if(Rec<=no):
+        print(Rec)
+        Rec=Rec+1
+        Display(no)
+
+def main():
+    print("Enter a Number :",end="")
+    Value=int(input())
+
+    Display(Value)
+   
+
+if __name__=="__main__":
+    main()
