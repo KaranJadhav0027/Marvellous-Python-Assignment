cd01297c50880e7ae94492841fd93520e7e6cab6
Karan Jadhav <142383819+KaranJadhav0027@users.noreply.github.com>
2025-06-07 00:06:11 +0530

    Add files via upload

---
+++ Assignment_14/Assignment14_2.py
@@ -0,0 +1,32 @@
+"""############################################################
+
+Area of Rectangle : 110
+
+Perimeter of Rectangle : 42
+
+###############################################################"""
+
+class Rectangle:
+    def __init__(self,Length,Width):
+        self.length=Length
+        self.width=Width
+
+    def RetArea(self):
+        area=self.width*self.length
+        return area
+    
+    def RetPerimeter(self):
+        perimeter=2*(self.width+self.length)
+        return perimeter
+
+def main():
+    Robj=Rectangle(11,10)
+    ARet=Robj.RetArea()
+    PRet=Robj.RetPerimeter()
+
+    print("Area of Rectangle :",ARet)
+    print("Perimeter of Rectangle :",PRet)
+
+
+if __name__=="__main__":
+    main()
---
+++ Assignment_14/Assignment14_5.py
@@ -0,0 +1,49 @@
+"""############################################################
+
+Account No : 987654
+Name : Karan
+Balance Amount : 20000
+
+Enter the Amount you want Deposit :23999
+
+Enter the Amount you want Withdraw :3456
+
+Account No : 987654
+Name : Karan
+Balance Amount : 40543
+
+###############################################################"""
+
+class BankAccount:
+    def __init__(self,account_number,name,balance):
+        self.Account_number=account_number
+        self.Name=name
+        self.Balance=balance
+    
+    def deposit(self):
+        print("Enter the Amount you want Deposit :",end="")
+        DAmount=int(input())
+        self.Balance=self.Balance+DAmount
+
+    def withdraw(self):
+        print("Enter the Amount you want Withdraw :",end="")
+        WAmount=int(input())
+        if WAmount<self.Balance:
+            self.Balance=self.Balance-WAmount
+        else:
+            print("Enter the less than ",self.Balance, "Amount")
+
+    def DisplayBalance(self):
+        print("Account No :",self.Account_number)
+        print("Name :",self.Name)
+        print("Balance Amount :",self.Balance)
+
+def main():
+   bobj=BankAccount(987654,"Karan",20000)
+   bobj.DisplayBalance()
+   bobj.deposit()
+   bobj.withdraw()
+   bobj.DisplayBalance()
+
+if __name__=="__main__":
+    main()
