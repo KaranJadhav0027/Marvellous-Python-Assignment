6e3bcc07080a7e44f9b70c461c74d9f462a21780
Karan Jadhav <142383819+KaranJadhav0027@users.noreply.github.com>
2025-07-09 15:49:45 +0530

    Add files via upload

---
+++ AIML-Code/DirectoryTraversalExtensionX.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import time
+
+def DirectoryWatcher(DirectoryName,Extension):
+    
+    flag=os.path.isabs(DirectoryName) #Check the path
+
+    if(flag ==False):
+         DirectoryName=os.path.abspath(DirectoryName) #give the path (updeter)
+
+    flag=os.path.exists(DirectoryName) #check the valid path
+
+    if(flag==False):
+         print("The path is invalid ")
+         exit()
+    
+    flag=os.path.isdir(DirectoryName) #check its is Directory or not 
+    
+    if(flag==False):
+         print("path is valid but the target is not a directory ")
+
+
+    for FolderName,SunFolderNames,FileNames in os.walk(DirectoryName):
+          for fname in FileNames:
+            #  fname=os.path.join(FolderName,fname)
+            if fname.endswith(Extension):
+               print(fname) 
+             
+  
+def main():
+        Border="-"*54
+        print(Border)
+        print("-----------------Marvellous Automation----------------")
+        print(Border)
+        
+        if(len(sys.argv)==2 ):
+            if((sys.argv[1]=="--h") or (sys.argv[1]=="--H")):
+                print("This application is used to perform Directory Cleaning")
+                print("This is the Directory automation script")
+
+            elif((sys.argv[1]=="--u") or (sys.argv[1]=="--U")):
+                print("Use the given script as ")
+                print("ScriptName.py  NameOfDirectory  Excepted_Extension")
+                print("Please provide Valid Absolute path")
+
+        elif(len(sys.argv)==3):
+            DirectoryWatcher(sys.argv[1],sys.argv[2])
+        
+        else:
+            print("Invalid number of Command line Arguments")
+            print("Use the given flage as :")
+            print("--h :Used to Display the help")
+            print("--u: Used to Display the Usage")
+        
+        print(Border)
+        print("------------Thank you for using our Script------------")
+        print("----------------Marvellous Infosystems----------------")
+        print(Border)
+
+
+if __name__=="__main__":
+    main()
