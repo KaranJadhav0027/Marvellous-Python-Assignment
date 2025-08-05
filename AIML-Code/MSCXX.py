77b8b0d04767e172acb95a48876fab992bcc01fc
Karan Jadhav <142383819+KaranJadhav0027@users.noreply.github.com>
2025-08-05 22:59:22 +0530

    Add files via upload

---
+++ AIML-Code/MSCXX.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.metrics import mean_squared_error,r2_score
+
+def main():
+    y_actual=[10,20,30,40,50]
+    y_predicted=[10,20,30,40,50]
+
+    MSE=mean_squared_error(y_actual,y_predicted)
+
+    RMSE=np.sqrt(MSE)
+
+    print("Actual Value :",y_actual)
+    print("Predicted Value :",y_predicted)
+    print("Mean Square Error :",MSE)
+    print("Root Mean Square Error :",RMSE)
+    
+if __name__=="__main__":
+    main()
