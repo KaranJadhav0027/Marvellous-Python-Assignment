17cb150a0ec5fbf2d90f8c50c75e843df6498280
Karan Jadhav <142383819+KaranJadhav0027@users.noreply.github.com>
2025-08-18 21:54:27 +0530

    Add files via upload

---
+++ AIML-Code/RSquare.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import r2_score
+
+def main():
+   y_actual=[100,200,300,400,500]
+
+   y_pred=[110,190,310,390,510]
+
+   r2=r2_score(y_actual,y_pred)
+
+   print("Actual Values :",y_actual)
+   print("Predicted Value :",y_pred)
+
+   print("R2 Value ",r2)
+    
+if __name__=="__main__":
+    main()
